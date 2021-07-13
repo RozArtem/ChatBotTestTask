5656d3dbd24c4689fe3910a8ac9a89484877e03f
RozArtem <rozum13art@gmail.com>
2021-07-13 20:12:20 +0300

    models to db conf added

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,9 @@
 
+import { Cashier } from '../models/cashier.model';
+import { Shop } from '../models/shop.model';
 import { Sequelize } from 'sequelize-typescript'
+import { CashRegister } from '../models/cash-register.model';
+
 
 export const  sequalize = new Sequelize({
   dialect: "postgres",
@@ -10,7 +14,7 @@ export const  sequalize = new Sequelize({
   username: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
- // models: [ ]
+  models: [ Shop, Cashier,  CashRegister]
 });
 
 
